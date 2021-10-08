d6926dc4f89450c78f55257a0ace626baf78b952
shazanfar <shazanfar@gmail.com>
2021-10-08 13:48:22 +0100

    clean initialise file

--- scripts/initialise.R
+++ scripts/initialise.R
@@ -11,8 +11,6 @@ library(StabMap)
 source("../scripts/adaptiveKNN.R")
 source("../scripts/batch_wrap_functions.R")
 source("../scripts/convenience_functions.R")
-# source("../scripts/StabMap_functions.R")
-# source("../scripts/StabMap_generalised_experimental.R")
 source("../scripts/comparison_functions.R")
 source("../scripts/subsetUncertainty.R")
 source("../scripts/tfidf.R")
@@ -22,8 +20,5 @@ library(nord)
 method_colours = c(nord(palette = "lumina")[c(4,2)],
                    nord(palette = "rocky_mountain")[c(1)],
                    nord(palette = "lake_superior")[c(3)],
-                   nord(palette = "lumina")[c(1)],
-                   nord(palette = "lumina")[c(5)],
-                   nord(palette = "lumina")[c(4)])
-names(method_colours) = c("StabMap", "PCA", "MultiMAP", "UINMF", "PC_separate", "StabMap_labels",
-                          "StabMap_unprojected")
+                   nord(palette = "lumina")[c(1)])
+names(method_colours) = c("StabMap", "PCA", "MultiMAP", "UINMF", "PC_separate")
